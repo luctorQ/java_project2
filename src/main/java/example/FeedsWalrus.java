8e2bbdfd4a4216cadbe9426851ac41ecc9f27101
PLUSZYNSKI <pluszynski@bleak.pl>
2017-09-26 16:51:00 +0100

    TODO

--- src/main/java/example/FeedsWalrus.java
+++ src/main/java/example/FeedsWalrus.java
@@ -6,7 +6,7 @@ import example.values.Walrus;
 public class FeedsWalrus {
 
 	OpensCan opensCan = new OpensCan();
-//todo fix it immediately
+//TODO fix it immediately
 	public void feed(Walrus gary, CannedWalrusFood can) {
 		gary.addToStomach(opensCan.open(can));
 	}
